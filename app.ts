d75d90b933d5beb158392f3f1e7d21a22654cf5b
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-10 17:31:21 -0500

    Checking out array typing

--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
+const person: { name: string; age: number; hobbies: string[] } = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"]
+};
 
+let favoriteActivites: string[];
+favoriteActivites = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
