c3541e76fdd702340969f8bc78bb2f8084d07052
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-13 10:26:31 -0500

    Setting up union type test

--- app.ts
+++ app.ts
@@ -10,34 +10,54 @@
 //   role: [2, "author"]
 // };
 
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR
+// enum Role {
+//   ADMIN,
+//   READ_ONLY,
+//   AUTHOR
+// }
+
+// const person = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: Role.ADMIN
+// };
+
+// //This will work, despite violating the tuple typing above
+// //This will not work with the enum value
+// //person.role.push("admin");
+
+// let favoriteActivites: string[];
+// favoriteActivites = ["Sports"];
+
+// console.log(person.name);
+
+// for (const hobby of person.hobbies) {
+//   console.log(hobby.toUpperCase());
+// }
+
+// if (person.role === Role.AUTHOR) {
+//   console.log("Author");
+// }
+
+/** Checking out UNION types */
+function combine(
+  input1: number | string,
+  input2: number | string
+): number | string {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN
-};
+let combinedAges = combine(20, 30);
 
-//This will work, despite violating the tuple typing above
-//This will not work with the enum value
-//person.role.push("admin");
+console.log(combinedAges);
 
-let favoriteActivites: string[];
-favoriteActivites = ["Sports"];
+combinedAges = combine("Cheese", "Fish");
 
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if (person.role === Role.AUTHOR) {
-  console.log("Author");
-}
-
-export {};
+console.log(combinedAges);
