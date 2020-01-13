ab64da451cf2044338f5f3fcb3e6c551bd20d99e
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-13 10:18:16 -0500

    Adding in enum type

--- app.ts
+++ app.ts
@@ -1,17 +1,31 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"]
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
+const person = {
   name: "Maximilian",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"]
+  role: Role.ADMIN
 };
 
-person.role.push("admin");
-//person.role[1] = 10;
+//This will work, despite violating the tuple typing above
+//This will not work with the enum value
+//person.role.push("admin");
 
 let favoriteActivites: string[];
 favoriteActivites = ["Sports"];
@@ -21,3 +35,9 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+if (person.role === Role.AUTHOR) {
+  console.log("Author");
+}
+
+export {};
