ec7e8ee809f7cbdf0c7d651cf86a6218ea6b8bef
206620219 <Brian.DeMaio@universalorlando.com>
2020-01-13 11:23:38 -0500

    Dealing with unknown types

--- app.ts
+++ app.ts
@@ -19,9 +19,16 @@ function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
 //States what type of function and return type there will be
 let combinedValues: (a: number, b: number) => number;
 combinedValues = add;
+
 /** This will not work, because it does not match the function return type */
 //combinedValues = printResult;
 
+//Callback typing call
 addAndHandle(10, 20, result => {
   console.log("The callback result is " + result);
 });
+
+//Unknown value type
+let userInput: unknown;
+userInput = 5;
+userInput = "Hello";
--- functions.ts
+++ functions.ts
@@ -7,7 +7,8 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
-function addAdHandle(n1: number, n2: number, cb: (num: number) => void) {
+//Callback types setup
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
   const result = n1 + n2;
   cb(result);
 }
@@ -17,10 +18,25 @@ function addAdHandle(n1: number, n2: number, cb: (num: number) => void) {
 
 //States what type of function and return type there will be
 let combinedValues: (a: number, b: number) => number;
-combineValues = add;
+combinedValues = add;
+
 /** This will not work, because it does not match the function return type */
 //combinedValues = printResult;
 
-addAdHandle(10, 20, result => {
+//Callback typing call
+addAndHandle(10, 20, result => {
   console.log("The callback result is " + result);
 });
+
+//Unknown value type
+//Requires type check to assign the unknown value to
+//another variable
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Hello";
+
+if (typeof userInput === "string") {
+  userName = userInput;
+}
